2ddc1d13e2a1c4b699b9220a58f0680e07794b0f
Cagri Portakalkoku <portakalkoku@gmail.com>
2021-02-17 16:00:52 +0300

    Initial Commit

---
+++ MovieLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLand
+//
+//  Created by Çağrı Portakalkökü on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
